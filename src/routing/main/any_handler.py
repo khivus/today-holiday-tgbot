298e09735c8f6ea7cfd4e19b0a0b348d0b172784
khivus <khivus2973@gmail.com>
2024-05-08 13:15:39 +0300

    v1.15.5 update
    откат, пока что

--- src/routing/main/any_handler.py
+++ src/routing/main/any_handler.py
@@ -10,24 +10,24 @@ from src.utility.chat_check import is_group_in_db
 from src.utility.page_builder import build_pages, get_holiday_message
 
 
-@main_router.message()
-async def process_about(message: types.Message) -> None:
+# @main_router.message()
+# async def process_about(message: types.Message) -> None:
     
-    if message.text == 'сегодня':
-        await process_holidays(message=message)
-    elif message.text == 'завтра':
-        await process_tomorrow(message=message)
-    elif '.' in message.text:
-        is_group_in_db(chat_id=message.chat.id)
+    # if message.text == 'сегодня':
+    #     await process_holidays(message=message)
+    # elif message.text == 'завтра':
+    #     await process_tomorrow(message=message)
+    # elif '.' in message.text:
+    #     is_group_in_db(chat_id=message.chat.id)
 
-        raw_date: list = message.text.split('.')
-        date = Date(day=int(raw_date[0]), month=int(raw_date[1]))
+    #     raw_date: list = message.text.split('.')
+    #     date = Date(day=int(raw_date[0]), month=int(raw_date[1]))
 
-        pages = await build_pages(date=date)
-        message_text = get_holiday_message(page_index=0, pages=pages, date=date)
-        keyboard = build_pages_keyboard(current_page_index=0, max_page_index=len(pages), date=date)
+    #     pages = await build_pages(date=date)
+    #     message_text = get_holiday_message(page_index=0, pages=pages, date=date)
+    #     keyboard = build_pages_keyboard(current_page_index=0, max_page_index=len(pages), date=date)
 
-        try:
-            await message.answer(text=message_text, reply_markup=keyboard)
-        except:
-            pass
+    #     try:
+    #         await message.answer(text=message_text, reply_markup=keyboard)
+    #     except:
+    #         pass
